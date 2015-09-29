e33e6681ae44ced3f835f28887c5977e144f0184
ni4ka7a <n.atanasow@yahoo.com>
2015-09-29 15:23:52 +0300

    Class SimpleCalculator added and a simple unit test added.

---
+++ AppVeyorDemo/HelloWorld-Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld_Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using HelloWorld;
+
+    [TestClass]
+    public class SimpleCalculatorTests
+    {
+        [TestMethod]
+        public void AddTwoNumbersShouldReturnCorrectResult()
+        {
+            var calculator = new SimpleCalculator();
+            var firstNumber = 5;
+            var secondNumber = 6;
+
+            var expectedResult = 11;
+            var result = calculator.Add(firstNumber, secondNumber);
+            Assert.AreEqual(expectedResult, result, "The result is {0}, which is incorrect value", result);
+        }
+    }
+}
---
+++ AppVeyorDemo/HelloWorld/SimpleCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    public class SimpleCalculator
+    {
+        public SimpleCalculator()
+        {
+        }
+
+        public double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
